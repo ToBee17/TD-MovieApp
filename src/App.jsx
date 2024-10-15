14c82e4668a3902d742a216f6dfe5b005902e39c
ToBee17 <mechainadam@gmail.com>
2024-10-15 10:02:36 +0200

    COMMIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
-
-
-import useFetch from './hook/useFetch';
+import useFetch from "./hook/useFetch";
 
 export default function App() {
-  const { data: showData, isLoading: isLoadingShow, error: showError } = useFetch('https://api.tvmaze.com/shows/41428');
+  const {
+    data: showData,
+    isLoading: isLoadingShow,
+    error: showError,
+  } = useFetch("https://api.tvmaze.com/shows/41428");
   console.log(showData);
+
+  const { data: showData2 } = useFetch("https://api.tvmaze.com/shows/41428/images"); 
+  console.log(showData2);
 
   if (isLoadingShow) {
     return <div>Loading...</div>;
@@ -16,9 +20,28 @@ export default function App() {
   }
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center p-10 gap-5">
-      <h1>{showData.name}</h1>
-      <img src={showData.image.medium} alt={showData.name} />
-    </div>
+    <section className="h-screen flex flex-col items-center gap-5">
+      <div 
+      className="h-[25%] w-full"
+      style={{
+        backgroundImage: `url(${showData2[6].resolutions.original.url})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}></div>
+      
+
+      <section className="flex px-5 py-2">
+        <div>
+          <h1>{showData.name}</h1>
+          <ul>
+            <p>{showData.language}</p>
+            <p>{showData.genres}</p>
+          </ul>
+        </div>
+        <img src={showData.image.medium} alt={showData.name} />
+      </section>
+      <h2>Background image</h2>
+      {/* <img src={showData2[5].resolutions.original.url} alt={showData.name} /> */}
+    </section>
   );
 }
