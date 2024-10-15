37f5b2db29097f80dc5c9615e4875bb1f5fdafbb
ToBee17 <mechainadam@gmail.com>
2024-10-15 09:13:19 +0200

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,18 @@ export default function App() {
   const { data: showData, isLoading: isLoadingShow, error: showError } = useFetch('https://api.tvmaze.com/shows/41428');
   console.log(showData);
 
+  if (isLoadingShow) {
+    return <div>Loading...</div>;
+  }
+
+  if (showError) {
+    return <div>Error: {showError.message}</div>;
+  }
+
   return (
     <div className="h-screen flex flex-col items-center justify-center p-10 gap-5">
       <h1>Hello</h1>
+      <p>Florian est le goat</p>
 
     </div>
   );
