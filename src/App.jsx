238bb1c8e566395ba12fd4ebe1a853d0a4a66f33
ToBee17 <mechainadam@gmail.com>
2024-10-17 09:42:50 +0200

    Format document

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ export default function App() {
 
   return (
     <section className="h-screen flex flex-col items-center gap-5 font-medium bg-background text-white">
+      
       <div
         className="h-[25%] w-full"
         style={{
@@ -32,21 +33,43 @@ export default function App() {
         }}
       ></div>
 
-        <div className="flex justify-between w-full px-5">
-          <section className="flex flex-col gap-5">
-            <h1 className="text-xl">{showData.name}</h1>
-            <ul>
-              <p><span className="text-span">Average Runtime : </span>{showData.averageRuntime}min</p>
-              <p><span className="text-span">Status : </span>{showData.status}</p>
-              <p><span className="text-span">Show Type : </span>{showData.type}</p>
-              <p><span className="text-span">Genres : </span>{showData.genres}</p>
-              <p><span className="text-span">Épisodes ordered : </span>{showData.status}</p>
-              <p><span className="text-span">How to watch : </span>{showData.webChannel.name}</p>
-            </ul>
-          </section>
-          <img src={showData.image.medium} alt={showData.name} className="max-w-[40%] rounded-lg outline outline-[.5px] outline-white shadow-md shadow-white"/>
-        </div>
-        
+      {/* Infos Movie */}
+      <div className="flex justify-between w-full px-5">
+        <section className="flex flex-col gap-5">
+          <h1 className="text-xl">{showData.name}</h1>
+          <ul>
+            <p>
+              <span className="text-span">Average Runtime : </span>
+              {showData.averageRuntime}min
+            </p>
+            <p>
+              <span className="text-span">Status : </span>
+              {showData.status}
+            </p>
+            <p>
+              <span className="text-span">Show Type : </span>
+              {showData.type}
+            </p>
+            <p>
+              <span className="text-span">Genres : </span>
+              {showData.genres}
+            </p>
+            <p>
+              <span className="text-span">Épisodes ordered : </span>
+              {showData.status}
+            </p>
+            <p>
+              <span className="text-span">How to watch : </span>
+              {showData.webChannel.name}
+            </p>
+          </ul>
+        </section>
+        <img
+          src={showData.image.medium}
+          alt={showData.name}
+          className="max-w-[40%] rounded-lg outline outline-[.5px] outline-white shadow-md shadow-white"
+        />
+      </div>
     </section>
   );
 }
