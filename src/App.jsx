dd7f507d5285af86e56317c3949f2620c90a4728
florianUnilim <florian.denis@etu.unilim.fr>
2024-10-17 09:29:16 +0200

    component cast 60%

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-
-
+import { ScrollArea } from "@/components/ui/scroll-area"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import useFetch from './hook/useFetch';
 
 export default function App() {
-  const { data: showData, isLoading: isLoadingShow, error: showError } = useFetch('https://api.tvmaze.com/shows/41428');
+  const { data: showData, isLoading: isLoadingShow, error: showError } = useFetch('https://api.tvmaze.com/shows/41428/cast');
   console.log(showData);
 
+  if (isLoadingShow) return <div>Chargement...</div>;
+  if (showError) return <div>Erreur 404 {showError.message}</div>;
+
   return (
-    <div className="h-screen flex  items-center justify-center p-10 gap-5">
-      <div src="https://api.tvmaze.com/the-queens-gambit/1/cast"></div>
-      <Avatar>
-  <AvatarImage src="https://github.com/shadcn.png" />
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-
-<Avatar>
-  <AvatarImage src="https://api.tvmaze.com/the-queens-gambit/140140/cast" />
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-
-<Avatar>
-  <AvatarImage src="https://github.com/shadcn.png" />
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-
-<Avatar>
-  <AvatarImage src="https://github.com/shadcn.png" />
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-
+    <div className="h-screen flex  items-center justify-center p-10 gap-5"> 
+      <div>
+      <h1 className="pl-10">cast</h1>
+      <ScrollArea className="pl-10 h-20 w-[440px] rounded-md mt-4">
+      <ul className="flex gap-2">
+        {showData && showData.map((castMember) => (
+          <li key={castMember.person.id} >
+            <Avatar>
+          <AvatarImage src={castMember.person.image.original}/>
+        </Avatar>
+        </li>
+        ))}
+      </ul>
+      </ScrollArea>
+    </div>
+    
     </div>
   );
 }
