b3794dfeb68b062bcda13d7b0bcdeb684d7d98dc
ToBee17 <mechainadam@gmail.com>
2024-10-24 15:45:13 +0200

    Avec Code de Florian

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,24 @@
 import useFetch from "./hook/useFetch";
+import { useState } from "react";
 
 import { Button } from "./components/ui/button";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "./components/ui/tabs";
-import {
-  Select,
-  SelectTrigger,
-  SelectContent,
-  SelectItem,
-} from "./components/ui/select";
+import { Select, SelectTrigger, SelectContent, SelectItem } from "./components/ui/select";
+import { Popover, PopoverTrigger, PopoverContent } from "./components/ui/popover";
+import { Avatar, AvatarImage } from "./components/ui/avatar";
+import { ScrollArea, ScrollBar} from "./components/ui/scroll-area";
 
 export default function App() {
+  const [selectedPersonId, setSelectedPersonId] = useState(null);
+
   const {
     data: showData,
     isLoading: isLoadingShow,
     error: showError,
   } = useFetch(
-    "https://api.tvmaze.com/shows/83?embed[]=images&embed[]=seasons&embed[]=episodes"
+    "https://api.tvmaze.com/shows/83?embed[]=images&embed[]=seasons&embed[]=episodes&embed[]=cast"
   );
   console.log(showData);
-
-  // const { data: showDataImage } = useFetch(
-  //   "https://api.tvmaze.com/shows/83/images"
-  // );
-  // console.log(showDataImage);
-
-  // const { data: showDataSeason } = useFetch(
-  //   "https://api.tvmaze.com/shows/83/seasons"
-  // );
-  // console.log(showDataSeason);
-
-  // const { data: showDataEpisode } = useFetch(
-  //   "https://api.tvmaze.com/shows/83/episodes"
-  // );
-  // console.log(showDataEpisode);
 
   if (isLoadingShow) {
     return <div>Loading...</div>;
@@ -100,7 +86,7 @@ export default function App() {
       </div>
 
       {/* Tabs */}
-      <Tabs defaultValue="episodes" className="w-full px-5 h-full md:z-10">
+      <Tabs defaultValue="infos" className="w-full px-5 h-full md:z-10">
         <TabsList className="w-full flex justify-around gap-1 mb-10">
           <TabsTrigger value="infos" className="w-full">
             Infos
@@ -132,6 +118,57 @@ export default function App() {
             >
               Watch the trailer
             </Button>
+          </div>
+
+
+          {/* Cast */}
+          <div className="bg-BackgroundColor font-main">
+            <div>
+              <h1 className="pl-10 text-TextColor text-2xl font-bold">
+                Note de la communauté: {showData.rating.average}/10
+              </h1>
+            </div>
+            <div>
+              <h1 className="pl-10 pt-7 text-TextColor text-2xl font-bold">
+                Cast
+              </h1>
+            </div>
+
+            <ScrollArea className="h-25 w-full rounded-md mt-4">
+              <div className="flex w-full space-x-4 p-4">
+                {showData._embedded.cast &&
+                  showData._embedded.cast.map((castMember) => (
+                    <figure key={castMember.person.id} className="shrink-0">
+                      <div className="overflow-hidden rounded-md">
+                        <Popover>
+                          <PopoverTrigger
+                            className="flex items-center"
+                            onClick={() =>
+                              setSelectedPersonId(castMember.person.id)
+                            }
+                          >
+                            <Avatar className="h-20 w-20">
+                              <AvatarImage
+                                src={castMember.person.image.medium}
+                                className="size-20 object-cover"
+                              />
+                            </Avatar>
+                          </PopoverTrigger>
+                          <PopoverContent>
+                            {selectedPersonId === castMember.person.id && (
+                              <PersonDetails
+                                personId={selectedPersonId}
+                                castMember={castMember}
+                              />
+                            )}
+                          </PopoverContent>
+                        </Popover>
+                      </div>
+                    </figure>
+                  ))}
+              </div>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
           </div>
         </TabsContent>
 
@@ -166,3 +203,33 @@ export default function App() {
     </section>
   );
 }
+
+// function PersonDetails({ personId, castMember }) {
+//   const {
+//     data: personData,
+//     isLoading: isLoadingPerson,
+//     error: personError,
+//   } = useFetchPerson(`https://api.tvmaze.com/people/${personId}`);
+
+//   if (isLoadingPerson) return <div>Chargement...</div>;
+//   if (personError) return <div>Erreur 404 {personError.message}</div>;
+
+//   return (
+//     <div className="flex flex-col items-center p-4 bg-background font-main">
+//       <img
+//         src={castMember.person.image.medium}
+//         className="rounded-xl"
+//         alt={castMember.person.name}
+//       />
+//       <h2 className="text-lg font-bold text-white ">
+//         {castMember.person.name}
+//       </h2>
+//       {personData && (
+//         <>
+//           <p className="text-white">{personData.birthday}</p>
+//           <p className="text-white">{personData.country.name}</p>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
